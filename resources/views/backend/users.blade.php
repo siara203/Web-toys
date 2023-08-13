9da59e192ada836593cf50663ef81e4030df21db
siara203 <sonpv00325@gmail.com>
2023-08-14 02:05:15 +0700

    update info

--- resources/views/backend/users.blade.php
+++ resources/views/backend/users.blade.php
@@ -59,26 +59,26 @@
                 </ul>
             </li>
             <li class=" ">
-                <a href="#purchase" class="collapsed" data-toggle="collapse" aria-expanded="false">
-                    <svg class="svg-icon" id="p-dash5" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect>
-                        <line x1="1" y1="10" x2="23" y2="10"></line>
+                <a href="#product" class="collapsed" data-toggle="collapse" aria-expanded="false">
+                    <svg class="svg-icon" id="p-dash2" width="20" height="20"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle>
+                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
                     </svg>
-                    <span class="ml-4">Order</span>
+                    <span class="ml-4">Orders</span>
                     <svg class="svg-icon iq-arrow-right arrow-active" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <polyline points="10 15 15 20 20 15"></polyline><path d="M4 4h7a4 4 0 0 1 4 4v12"></path>
                     </svg>
                 </a>
-                <ul id="purchase" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
-                        <li class="">
-                                <a href="#">
-                                    <i class="las la-minus"></i><span>List Order</span>
-                                </a>
-                        </li>
-                        <li class="">
-                                <a href="#">
-                                    <i class="las la-minus"></i><span>Add Order</span>
-                                </a>
-                        </li>
+                <ul id="product" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
+                    <li class="">
+                        <a href="#">
+                            <i class="las la-minus"></i><span>List Order</span>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="#">
+                            <i class="las la-minus"></i><span>Add Order</span>
+                        </a>
+                    </li>
                 </ul>
             </li>
         </ul>
